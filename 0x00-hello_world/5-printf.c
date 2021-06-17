1a3e586e00cabc55e6c3bbecc62d47bcf48d71af
Damilola Akinyele <donjohndam@gmail.com>
2021-06-17 07:00:04 -0700

    low level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+      /**
+       * Main - Entry point
+       *
+       * Return: Always 0 (success)
+       */
+int main(void)
+{
+printf("with proper grammar, but theoutcome is a piece of art, \n");
+return (0);
+}
