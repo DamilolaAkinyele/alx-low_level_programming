e2171b3eb3a3d758368914813439ef05b69f50a3
Damilola Akinyele <donjohndam@gmail.com>
2021-06-17 09:18:46 -0700

    low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
       /**
        * Main - Entry point
@@ -7,6 +6,6 @@
        */
 int main(void)
 {
-printf("with proper grammar, but theoutcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
