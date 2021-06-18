85141a631f02fb5e485f4f2121427b1a8a0ea431
Damilola Akinyele <donjohndam@gmail.com>
2021-06-18 05:24:43 -0700

    variables, if, else and while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *Main - Determines either greater than 5, is less than 6, or is 0
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar("\n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *Main - prints the alphabets to exclude q and e.
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+if (i != 101 && != 113)
+{
+putchar(i);
+}
+}
+putchar("\n");
+return (0);
+}
