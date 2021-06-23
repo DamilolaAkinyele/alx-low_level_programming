e10838eed580def35663c82994bc5002598800f4
Damilola Akinyele <donjohndam@gmail.com>
2021-06-23 02:51:19 -0700

    header file added

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,14 +1,17 @@
-#ifndef_holberton_h_
-#define_holberton_h_
+#ifndef _holberton_h_
+#define _holberton_h_
 
-int_putchar(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-
-int -putchar(char c)
-{
-return (write (1, &c, 1));
-}
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
