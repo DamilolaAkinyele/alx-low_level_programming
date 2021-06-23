f59d735c820340b6c7412debf9362851a506021f
Damilola Akinyele <donjohndam@gmail.com>
2021-06-23 02:29:18 -0700

    header file added

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,14 @@
+#ifndef_holberton_h_
+#define_holberton_h_
+
+int_putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int_islower(int c);
+
+int -putchar(char c)
+{
+return (write (1, &c, 1));
+}
+
+#endif
