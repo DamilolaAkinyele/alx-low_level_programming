7b460a3ba6daee9efd131ce0de2abc1acd215fb9
Damilola Akinyele <84016682+DamilolaAkinyele@users.noreply.github.com>
2021-06-29 13:46:43 +0100

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,12 +5,10 @@
 * @s: input string.
 * Return: length of a string.
 */
-int _strlen(char *s) {
-int c = 0;
-
-while(*s[c] != '\0')
-c++;
-
-return c;
+int _strlen(char *s)
+{
+int count = 0;
+while (*(s + count) != '\0')
+count++;
+return (count);
 }
-
