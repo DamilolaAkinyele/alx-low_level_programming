b86c5e8863c0fe73374b8e659ec4a3307ec1b05c
Damilola Akinyele <84016682+DamilolaAkinyele@users.noreply.github.com>
2021-07-22 16:26:22 +0100

    sum them all updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,14 +12,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
-int i, sum;
-
-va_start (ap, n);         /* Initialize the argument list. */
-
-sum = 0;
+unsigned int i;
+int sum = 0;
 
 if (n == 0)
 return (0);
+
+va_start (ap, n);         /* Initialize the argument list. */
 
 for (i = 0; i < n; i++)
 sum += va_arg (ap, int);    /* Get the next argument value. */
