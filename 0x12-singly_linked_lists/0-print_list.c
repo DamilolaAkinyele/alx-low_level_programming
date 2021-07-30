086ee1c4f565ed3eb937e2a0b8af5b548c0123d9
Damilola Akinyele <84016682+DamilolaAkinyele@users.noreply.github.com>
2021-07-30 12:12:31 +0100

    print list updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,18 @@
                                                                                                      
 size_t print_list(const list_t *h)                                                                   
 {                                                                                                    
-size_t count = 0;                                                                                    
-while (h != NULL)                                                                                    
-if (h->str == NULL)                                                                                  
-printf("[%d] %s\n", 0, "(nil)");                                                                     
-else                                                                                                 
-(printf("[%d] %s", h->data, h->str);                                                                 
-h = h->next;                                                                                         
-count++;                                                                                             
-}                                                                                                    
-return (count);                                                                                      
+size_t count;
+
+count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
 }
 
